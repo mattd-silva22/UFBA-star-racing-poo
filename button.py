ab9274170172f6762d26999a00aa62fe6e0c39c8
pricaraujo <pri11araujo41@gmail.com>
2023-07-04 14:01:28 -0300

    adiciona contagem, faz refatoração de código com boas práticas, encapsulamento e tipagem estática

--- button.py
+++ button.py
@@ -2,21 +2,27 @@ from tupy import *
 from time import sleep
 
 class Button(Image):
-    def __init__(self, x, y, file):
+    def __init__(self, x: int, y: int, file: str) -> None:
         self.file = file
         self.x = x
         self.y = y
         self.is_clicked = False
 
-    def update(self):
+    def update(self) -> None:
         if (self.x <  mouse.x < self.x + 80) and (self.y < mouse.y < self.y + 80):
             if mouse.is_button_down():
                 sleep(0.01)
                 self.click()
 
-    def click(self):
+    def click(self) -> None:
         self.is_clicked = not self.is_clicked
-
+    
+    def get_state(self) -> bool:
+        return self.is_clicked
+    
+    def reset_state(self) -> None:
+        self.is_clicked = False
+        
 if __name__ == '__main__':
     pause_button = Button("Pausar", 10, 10, "pausa.png")
     restart_button = Button("Reiniciar", 10, 10, "return.png")
--- star.py
+++ star.py
@@ -2,7 +2,7 @@ from tupy import *
 from time import sleep
 
 class Star(Image):
-    def __init__(self, star):
+    def __init__(self, star) -> None:
         self.file = "Stars/star.png"
         self.x = (star + 1) * 40
         self.y = 450
